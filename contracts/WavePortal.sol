acb8991ae6f86e8b19daaa42e46bf860eff6bf2a
Eduardo V. Bruno <eduardo.bruno@fauna.com>
2022-06-15 13:52:46 -0300

    waving

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract WavePortal {
+    uint256 totalWaves;
+    address[] wavers;
+
     constructor() {
         console.log("Yo yo, I am a contract and I am smart");
+    }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+        wavers.push(msg.sender); 
+    }
+
+    function getTotalWaves() public view returns (uint256) {
+        console.log("We have %d total waves, from %d wavers !", totalWaves, wavers.length);
+        return totalWaves;
     }
 }
